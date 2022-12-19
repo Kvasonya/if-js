75626d0f2e19e3606937bf185f4ac96cb60fce82
Anna Kvasova <a.kvasova91@gmail.com>
2022-12-19 03:43:54 +0300

    added arrays, calculated the result of multiplying the arrayOne data and selected the necessary data in the arrayTwo

--- main.js
+++ main.js
@@ -1,64 +1,70 @@
-const person = {
-  name: 'Nick',
-  age: 25,
-  children: [
-    {
-      name: 'Mike',
-      age: 3,
-    },
-    {
-      name: 'Sarah',
-      age: 5,
-    }
-  ]
-};
+const line = '-----';
 
-console.log(person)
-console.log(person.children[1])
-
-
-//Homework
-//task 1
-
+//--------ex 1---------
 let user = 'John Doe';
-console.log(user)
 
-let student = 'Anna Kvasova'
-console.log(student)
+console.log(user);
 
-user = student
+let student = 'Anna Kvasova';
+
+console.log(student);
+
+user = student;
+
 //Anna Kvasova
-console.log(user)
+console.log(user);
 
+console.log(line);
 
-//task 2
+//--------ex 2---------
 let test = 1;
-console.log(test)
 
-test  += 1;
+test += 1;
+
 //2
-console.log(test)
+console.log(test);
 
-test += '1'
+test += '1';
+
 //21
-console.log(test)
+console.log(test);
 
-test -= 1
+test -= 1;
+
 //20
-console.log(test)
+console.log(test);
 
-test = true
+test = true;
+
 //true
-console.log(test)
+console.log(test);
 
-//task 3
-const arrayOne = [
-  2, 3, 5, 8
-    ]
+console.log(line);
 
-console.log(arrayOne)
+//--------ex 3---------
+const arrayOne = [2, 3, 5, 8];
+let result = 1;
 
-const arrayTwo = [2, 5, 8, 15, 0, 6, 20, 3
-]
+for (let i = 0; i < arrayOne.length; i++) {
+  result = result * arrayOne[i];
+}
 
-console.log(arrayTwo)
+console.log(result);
+
+console.log(line);
+
+const arrayTwo = [2, 5, 8, 15, 0, 6, 20, 3];
+
+for (let i = 0; i < arrayTwo.length; i++) {
+  if (arrayTwo[i] > 5 && arrayTwo[i] < 10) {
+    console.log(arrayTwo[i]);
+  }
+}
+
+console.log(line);
+
+for (let i = 0; i < arrayTwo.length; i++) {
+  if (arrayTwo[i] !== 0 && arrayTwo[i] % 2 === 0) {
+    console.log(arrayTwo[i]);
+  }
+}
