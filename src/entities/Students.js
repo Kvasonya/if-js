e8dbfaecc19ce0c708302ea3c37afd78ee931f10
Anna Kvasova <a.kvasova91@gmail.com>
2023-02-23 22:20:21 +0300

    edited class  Students

--- src/entities/Students.js
+++ src/entities/Students.js
@@ -12,24 +12,24 @@ export default class Students {
         }),
     );
   }
+
+  getInfo() {
+    return this.students
+      .sort((a, b) => a.course - b.course)
+      .filter((student) => student.course >= 1 && student.course <= 5)
+      .map(
+        (student) =>
+          `${student.fullName} - ${student.courseName}, ${student.course} курс`,
+      );
+  }
+
+  getInfoGraduates() {
+    return this.students
+      .sort()
+      .filter((student) => student.course > 5)
+      .map(
+        (student) =>
+          `${student.fullName} - ${student.courseName}, год выпуска: ${student.graduation}`,
+      );
+  }
 }
-
-Students.prototype.getInfo = function () {
-  return this.students
-    .sort((a, b) => a.course - b.course)
-    .filter((student) => student.course >= 1 && student.course <= 5)
-    .map(
-      (student) =>
-        `${student.fullName} - ${student.courseName}, ${student.course} курс`,
-    );
-};
-
-Students.prototype.getInfoGraduates = function () {
-  return this.students
-    .sort()
-    .filter((student) => student.course > 5)
-    .map(
-      (student) =>
-        `${student.fullName} - ${student.courseName}, год выпуска: ${student.graduation}`,
-    );
-};
