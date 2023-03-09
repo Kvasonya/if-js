5f008ac44332c029438af87bf5dce21d38e3ee66
Anna Kvasova <a.kvasova91@gmail.com>
2023-03-09 20:02:34 +0300

    edited main.js (triphouse), add only slider in js; edited styles

--- triphouse/src/scripts/main.js
+++ triphouse/src/scripts/main.js
@@ -65,176 +65,168 @@ const data = [
   },
 ];
 
-//-----функции-шаблоны для переиспользования
+//-----version 1
 
-const createContainer = (...className) => {
-  const container = document.createElement('div');
-  container.classList.add('container', ...className);
-  return container;
-};
-
-const createDiv = (...className) => {
-  const div = document.createElement('div');
-  div.classList.add(...className);
-  return div;
-};
-
-const createImg = (src, alt, ...className) => {
-  const img = document.createElement('img');
-  img.setAttribute('src', src);
-  img.setAttribute('alt', alt);
-  img.classList.add(...className);
-  return img;
-};
-
-const createLink = (src, text, ...className) => {
-  const link = document.createElement('a');
-  link.setAttribute('src', src);
-  link.innerHTML = text;
-  link.classList.add(...className);
-  return link;
-};
-
-const createLinkImg = (src, ...className) => {
-  const link = document.createElement('a');
-  link.setAttribute('src', src);
-  link.classList.add(...className);
-  return link;
-};
-
-const createEl = (tagName, ...className) => {
-  const el = document.createElement(tagName);
-  el.classList.add(...args);
-  return el;
-};
-
-const createTitle = (range, text, ...className) => {
-  const title = document.createElement(`h${range}`);
-  const titleText = document.createTextNode(text);
-  title.classList.add(...className);
-  title.prepend(titleText);
-
-  return title;
-};
-
-const createText = (content, ...className) => {
-  const text = document.createElement('p');
-  const textContent = document.createTextNode(text);
-  text.classList.add(...className);
-  text.prepend(textContent);
-
-  return text;
-};
-
-//-----roundButton для переиспользования (что-то не так; поправить!)
-
-const roundButton = (...className) => {
-  const roundButton = createEl('button', 'slider__round-button', 'button');
-  const roundButtonDiv = createDiv(
-    'slider__round-button-arrow',
-    'arrow-figure',
-  );
-  const roundButtonLineTop = createEl(
-    'i',
-    'arrow-figure__top',
-    'arrow-figure__line',
-    'hotels-preferred__slider-round-button',
-  );
-  const roundButtonLineBottom = createEl(
-    'i',
-    'arrow-figure__bottom',
-    'arrow-figure__line',
-    'hotels-preferred__slider-round-button',
-  );
-
-  roundButton.setAttribute('type', 'button');
-
-  roundButtonDiv.prepend(roundButtonLineTop, roundButtonLineBottom);
-  roundButton.prepend(roundButtonDiv);
-
-  roundButton.classList.add(...className);
-
-  return roundButton;
-};
-
-//-----Homes Guests Loves
-
-const sectionHotels = (data) => {
-  const container = createContainer();
-  const section = createEl(
-    'section',
-    'hotels-preferred',
-    '_bg-main',
-    'col-xl-12',
-    'col-lg-12',
-    'col-md-12',
-    'col-sm-6',
-    'col-xs-6',
-  );
-  const content = createDiv('hotels-preferred__container');
-  const slider = createDiv('hotels__slider', 'slider');
-  const title = createTitle(
-    '2',
-    'Homes guests loves',
-    'hotels-preferred__title',
-    'title',
-  );
+const hotelsSlider = () => {
+  const slider = document.getElementById('js-hotels-slider');
 
   data.slice(0, 4).forEach((item) => {
-    const slide = createDiv(
-      'hotels-preferred__slider-content',
-      'slider__content',
-    );
+    //-----как сделать адаптив?
+    const slide = document.createElement('div');
+    slide.classList.add('.hotels-preferred__slider-content', 'slider__content');
+
     slider.appendChild(slide);
 
-    const linkImg = createLinkImg('#', 'hotels-preferred__slider-content_link');
+    const linkImg = document.createElement('a');
+    linkImg.setAttribute('href', '#');
+    linkImg.classList.add('hotels-preferred__slider-content_link');
 
-    const img = createImg(
-      `${item.imageUrl}`,
-      `${item.name}, ${item.city}, ${item.country}`,
+    const img = document.createElement('img');
+    img.setAttribute('src', `${item.imageUrl}`);
+    img.setAttribute('alt', `${item.name}, ${item.city}, ${item.country}`);
+    img.classList.add(
       'hotels-preferred__slider-content_img',
       'slider__content_img',
     );
 
     linkImg.prepend(img);
 
-    const hotelName = createLink(
-      '#',
-      `${item.name}`,
+    const hotelName = document.createElement('a');
+    hotelName.setAttribute('href', '#');
+    hotelName.classList.add(
       'hotels-preferred__slider-content_link',
       'link',
       'slider__content_link',
       'slider__content_link-main',
     );
-    const hotelLocation = createLink(
-      '#',
-      `${item.city}, ${item.country}`,
+
+    const hotelNameText = document.createTextNode(`${item.name}`);
+    hotelName.prepend(hotelNameText);
+
+    const hotelLocation = document.createElement('a');
+    hotelLocation.setAttribute('href', '#');
+    hotelLocation.classList.add(
       'hotels-preferred__slider-content_link',
       'link',
       'slider__content_link',
       'slider__content_link-secondary',
     );
 
+    const hotelLocationText = document.createTextNode(
+      `${item.city}, ${item.country}`,
+    );
+    hotelLocation.prepend(hotelLocationText);
+
     slide.prepend(linkImg, hotelName, hotelLocation);
+    slider.appendChild(slide);
   });
-
-  container.prepend(title, content);
-  content.prepend(
-    roundButton(
-      'hotels-preferred__slider-round-button',
-      'hotels-preferred__slider-round-button-next',
-      'swiper-button-next',
-    ),
-    slider,
-  );
-
-  section.prepend(container);
-
-  return section;
 };
 
-//-----сборка
+hotelsSlider();
 
-const main = document.querySelector('.triphouse');
+//-----or version 2 (через функции-шаблоны)
+//
+// const createContainer = (...className) => {
+//   const container = document.createElement('div');
+//   container.classList.add('container', ...className);
+//   return container;
+// };
+//
+// const createDiv = (...className) => {
+//   const div = document.createElement('div');
+//   div.classList.add(...className);
+//   return div;
+// };
+//
+// const createImg = (src, alt, ...className) => {
+//   const img = document.createElement('img');
+//   img.setAttribute('src', src);
+//   img.setAttribute('alt', alt);
+//   img.classList.add(...className);
+//   return img;
+// };
+//
+// const createLink = (src, text, ...className) => {
+//   const link = document.createElement('a');
+//   link.setAttribute('src', src);
+//   link.innerHTML = text;
+//   link.classList.add(...className);
+//   return link;
+// };
+//
+// const createLinkImg = (src, ...className) => {
+//   const link = document.createElement('a');
+//   link.setAttribute('src', src);
+//   link.classList.add(...className);
+//   return link;
+// };
+//
+// const createEl = (tagName, ...className) => {
+//   const el = document.createElement(tagName);
+//   el.classList.add(...className);
+//   return el;
+// };
+//
+// const createTitle = (range, text, ...className) => {
+//   const title = document.createElement(`h${range}`);
+//   const titleText = document.createTextNode(text);
+//   title.classList.add(...className);
+//   title.prepend(titleText);
+//
+//   return title;
+// };
+//
+// const createText = (content, ...className) => {
+//   const text = document.createElement('p');
+//   const textContent = document.createTextNode(text);
+//   text.classList.add(...className);
+//   text.prepend(textContent);
+//
+//   return text;
+// };
 
-main.appendChild(sectionHotels(data));
+// const hotelsSlider = () => {
+//   const slider = document.getElementById('js-hotels-slider');
+//
+//   data.slice(0, 4).forEach((item) => {
+//     const slide = createDiv(
+//       'hotels-preferred__slider-content',
+//       'slider__content',
+//     );
+//
+//     slider.appendChild(slide);
+//
+//     const linkImg = createLinkImg('#', 'hotels-preferred__slider-content_link');
+//
+//     const img = createImg(
+//       `${item.imageUrl}`,
+//       `${item.name}, ${item.city}, ${item.country}`,
+//       'hotels-preferred__slider-content_img',
+//       'slider__content_img',
+//     );
+//
+//     linkImg.prepend(img);
+//
+//     const hotelName = createLink(
+//       '#',
+//       `${item.name}`,
+//       'hotels-preferred__slider-content_link',
+//       'link',
+//       'slider__content_link',
+//       'slider__content_link-main',
+//     );
+//     const hotelLocation = createLink(
+//       '#',
+//       `${item.city}, ${item.country}`,
+//       'hotels-preferred__slider-content_link',
+//       'link',
+//       'slider__content_link',
+//       'slider__content_link-secondary',
+//     );
+//
+//     slide.prepend(linkImg, hotelName, hotelLocation);
+//     slider.appendChild(slide);
+//   });
+// };
+//
+// hotelsSlider();
